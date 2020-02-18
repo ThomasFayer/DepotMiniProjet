6a2bcd7ca5fde5b17e0027d31586dec3ad58a9cf
thomas <thomas.suzanne@lyceestvincent.fr>
2020-02-18 09:43:10 +0100

    salut

---
+++ connexion.php
@@ -0,0 +1,9 @@
+*<?php
+	$titre = 'Connexion';
+
+	include_once'./includes/header.php';	
+?>
+<form action="#" method="POST">
+	<label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+</form>
